6ab88aef82aab92bee48ccee620de97995403e5a
Schalk Burger <schalkb@gmail.com>
2023-08-10 12:44:24 +0200

    Update with themes

--- script.js
+++ script.js
@@ -89,3 +89,42 @@ document.getElementById("timerReset").addEventListener("click", () => {
   updateDocumentTitle("Productivity Timer"); // Set title to "Productivity Timer"
   timerElement.textContent = "25:00";
 });
+
+// Theme selector
+
+const pressedButtonSelector = '[data-theme][aria-pressed="true"]';
+const defaultTheme = "blue";
+
+const applyTheme = (theme) => {
+  const target = document.querySelector(`[data-theme="${theme}"]`);
+  document.documentElement.setAttribute("data-selected-theme", theme);
+  document.querySelector(pressedButtonSelector).setAttribute("aria-pressed", "false");
+  target.setAttribute("aria-pressed", "true");
+};
+
+const handleThemeSelection = (event) => {
+  const target = event.target;
+  const isPressed = target.getAttribute("aria-pressed");
+  const theme = target.getAttribute("data-theme");
+
+  if (isPressed !== "true") {
+    applyTheme(theme);
+    localStorage.setItem("selected-theme", theme);
+  }
+};
+
+const setInitialTheme = () => {
+  const savedTheme = localStorage.getItem("selected-theme");
+  if (savedTheme && savedTheme !== defaultTheme) {
+    applyTheme(savedTheme);
+  }
+};
+
+setInitialTheme();
+
+const themeSwitcher = document.querySelector(".theme-switcher");
+const buttons = themeSwitcher.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleThemeSelection);
+});
